5744f275042f19b95232089b48b661faf6674c46
axywego <pukeotim@gmail.com>
2026-07-28 16:16:39 +0300

    add database connection setup

--- app/main.py
+++ app/main.py
@@ -1,5 +1,10 @@
 from fastapi import FastAPI
 
+from app.database.base import Base
+from app.database.session_db import engine
+
+Base.metadata.create_all(bind=engine)
+
 app = FastAPI()
 
 @app.get("/")
