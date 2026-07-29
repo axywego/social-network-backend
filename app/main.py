5f1946b424cf053292aff638ab4e7eae6fe3710b
axywego <pukeotim@gmail.com>
2026-07-29 16:59:14 +0300

    fix router /auth/logout; fix import in /users; fix Optional in schema UserOut

--- app/main.py
+++ app/main.py
@@ -3,6 +3,8 @@ from fastapi import FastAPI
 from app.database.base import Base
 from app.database.session_db import engine
 
+from app.routers import auth, users
+
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
@@ -11,3 +13,5 @@ app = FastAPI()
 def home():
     return {"message": "Hello, World!"}
 
+app.include_router(auth.router)
+app.include_router(users.router)
