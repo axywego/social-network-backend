e65a04423797fff1668a3f6761b29a1a20d2a708
axywego <pukeotim@gmail.com>
2026-08-06 15:56:29 +0300

    add models chat, chat_user and chat_message for chatting

---
+++ app/models/chat.py
@@ -0,0 +1,29 @@
+import uuid
+from sqlalchemy import Column, String, DateTime, ForeignKey, CheckConstraint, Index, text
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.sql import func
+from app.database.base import Base
+
+class Chat(Base):
+    __tablename__ = "chats"
+
+    id = Column(UUID(as_uuid=True), primary_key=True, server_default=text("gen_random_uuid()"))
+    type = Column(String, nullable=False)
+    user_a_id = Column(UUID(as_uuid=True), ForeignKey("users.id"))
+    user_b_id = Column(UUID(as_uuid=True), ForeignKey("users.id"))
+    
+    created_at = Column(DateTime(timezone=True), nullable=False, server_default=func.now())
+
+    __table_args__ = (
+        CheckConstraint("type in ('direct', 'group')", name="check_type_valid"),
+        CheckConstraint("""
+            (type = 'direct' and user_a_id is not null and user_b_id is not null and user_a_id < user_b_id)
+            or
+            (type = 'group' and user_a_id is null and user_b_id is null)""", name="check_valid_table_variables"),
+        Index(
+            "idx_unique_direct_chat",
+            "user_a_id", "user_b_id",
+            unique=True,
+            postgresql_where=(type == "direct"),
+        ),
+    )
