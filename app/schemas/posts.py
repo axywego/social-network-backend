f024ff5fef4ecb5b6d4ef1967ce58c47ccd285a7
axywego <pukeotim@gmail.com>
2026-08-08 17:51:33 +0300

    added router posts and schemes for its

---
+++ app/schemas/posts.py
@@ -0,0 +1,25 @@
+from pydantic import BaseModel, EmailStr, ConfigDict, Field
+from typing import Optional
+
+from datetime import date, datetime
+from uuid import UUID
+
+class PostCommentCreate(BaseModel):
+    post_id: int
+
+    content: Optional[str] = None
+    image_url: Optional[str] = None
+
+class PostCreate(BaseModel):
+    content: Optional[str] = None
+    image_url: Optional[str] = None
+
+class PostOut(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
+    post_id: int
+    user_id: UUID
+    content: Optional[str] = None
+    image_url: Optional[str] = None
+
+    created_at: datetime
